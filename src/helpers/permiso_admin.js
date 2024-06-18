2caf7077a8f4ff41afff4e122a4f336b86f9d839
AzureAD\10800 <os.miguelparedes@gmail.com>
2024-06-18 18:26:06 -0500

    query cliente

--- src/helpers/permiso_admin.js
+++ src/helpers/permiso_admin.js
@@ -3,9 +3,19 @@ const Registro = require('../models/login.js');
 
 // Metodo para proteger las rutas del administrador
 const verificadoAdministrador = async (req, res, next) => {
-  const cliente = req.body.cliente
-  // Buscamos en el la bdd si el administrador inicio sesion o no
-  const sesion = await Registro.findOne({ _id : cliente })
+  let sesion = ''
+  const clientebody = req.body.cliente
+  if(!clientebody || clientebody.length === 0) {console.log('No hay id cliente en el body')}
+  else{
+    // Buscamos en el la bdd si el administrador inicio sesion o no
+    sesion = await Registro.findOne({ _id : clientebody })
+  }
+  const clientequery = req.query.cliente
+  if(!clientequery || clientequery.length === 0) {console.log('No hay id cliente en el query')}
+  else{
+    // Buscamos en el la bdd si el administrador inicio sesion o no
+    sesion = await Registro.findOne({ _id : clientequery })
+  }
   if(!sesion || sesion.length === 0) return res.redirect(`${process.env.URL}/login`);
   // Si existe un inicio de sesion del administrador
   if(sesion.inicioSesion == true && sesion.admin == true){
